751135e437b75fee9fdb91a88853d0c7efe3329b
KashyapHegdeKota <kota2022kashyap@gmail.com>
2025-10-29 18:14:56 -0700

    FIX: Fixed HTMLInputElement | null returning void.

--- src/pages/AuthenticationPage.tsx
+++ src/pages/AuthenticationPage.tsx
@@ -185,7 +185,7 @@ const OTPVerificationPage = () => {
               {otp.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => { inputRefs.current[index] = el; }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
